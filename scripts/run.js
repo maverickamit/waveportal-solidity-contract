ab2a8f56ee0cf0ca5611bc5f321e41b052c41de3
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-12-07 22:52:38 +0530

    update run.js to test waving by random person

--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,6 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+
   //Compile it.
   const ProjectContractFactory = await hre.ethers.getContractFactory(
     "MyProjectContract"
@@ -7,10 +9,26 @@ const main = async () => {
   const ProjectContract = await ProjectContractFactory.deploy();
   //Wait for it to be deployed/mined.
   await ProjectContract.deployed();
+
   console.log("Contract has deployed!! Address:", ProjectContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await ProjectContract.getTotalWaves();
+  console.log("Total Waves:", waveCount);
 
   let waveTxn = await ProjectContract.wave();
   await waveTxn.wait();
+
+  waveCount = await ProjectContract.getTotalWaves();
+  console.log("Total Waves:", waveCount);
+
+  //Waving by random person
+  waveTxn = await ProjectContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await ProjectContract.getTotalWaves();
+  console.log("Total Waves:", waveCount);
 };
 
 const runMain = async () => {
