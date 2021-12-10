2c950664021cabcf07245cc8a328627a923f78b7
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-12-10 15:40:36 +0530

    update contract to implement storing messages

--- contracts/MyProjectContract.sol
+++ contracts/MyProjectContract.sol
@@ -5,17 +5,32 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyProjectContract {
-    uint totalWaves;
+    uint totalWaves;    
+    
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; //Address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The time when the user waved. 
+    }
+    
+    Wave[] waves; // A variable that stores an array of structs.
+
     constructor(){
         console.log("Hello World.");
     }
     //POST
-    function wave() public{
+    function wave(string memory _message) public {
         totalWaves +=1;
-        console.log("Just Waved. totalWaves = %s", totalWaves);
-        console.log("Waved by = %s", msg.sender);
-
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
+    //GET
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;        
+    }
+
     //GET
     function getTotalWaves() public view returns (uint) {
         return totalWaves;        
