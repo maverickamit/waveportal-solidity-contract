b41bcaa2cc2f58615da221cb4861d0b2baabfdeb
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-12-12 14:06:08 +0530

    update run.js to test updated contract - send eth to users who wave and check updated contract balance

--- scripts/run.js
+++ scripts/run.js
@@ -1,3 +1,5 @@
+const { ethers } = require("ethers");
+
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
 
@@ -6,29 +8,55 @@ const main = async () => {
     "MyProjectContract"
   );
   //Deploy it.
-  const ProjectContract = await ProjectContractFactory.deploy();
+  const ProjectContract = await ProjectContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
   //Wait for it to be deployed/mined.
   await ProjectContract.deployed();
 
   console.log("Contract has deployed!! Address:", ProjectContract.address);
   console.log("Contract deployed by:", owner.address);
 
+  //Get the contract balance
+  let contractBalance = await hre.ethers.provider.getBalance(
+    ProjectContract.address
+  );
+  console.log(
+    "Contract Balance: ",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  //Get total waves
   let waveCount;
   waveCount = await ProjectContract.getTotalWaves();
   console.log("Total Waves:", waveCount.toString());
 
+  //Send wave
   let waveTxn = await ProjectContract.wave("Some message from me.");
   await waveTxn.wait();
 
+  //Get total waves
   waveCount = await ProjectContract.getTotalWaves();
   console.log("Total Waves:", waveCount.toString());
+
+  //Get the contract balance
+  contractBalance = await hre.ethers.provider.getBalance(
+    ProjectContract.address
+  );
+  console.log(
+    "Contract Balance: ",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   //Waving by random person
   waveTxn = await ProjectContract.connect(randomPerson).wave(
     "Some message from random person."
   );
   await waveTxn.wait();
+
   let allWaves = await ProjectContract.getAllWaves();
+
+  //Get total waves
   waveCount = await ProjectContract.getTotalWaves();
   console.log("Total Waves:", waveCount.toString());
   console.log(allWaves);
